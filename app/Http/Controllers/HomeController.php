2a18cffc0f3c30350d9d80fa34004a35af48e7d6
Arnontid <615021000620@mail.rmutk.ac.th>
2021-01-20 10:51:24 +0700

    Lap

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    function HelloMorning($ID){
+        return '<h1>GOOD0 MORNING'.$ID.'</h1>';
+    }
+}
